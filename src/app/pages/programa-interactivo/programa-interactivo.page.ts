cd753907ba979697a78d08241482142a11e929a1
Eduardo <eduardoresendiz5491@gmail.com>
2020-01-08 09:01:45 -0600

    Se sube tabla dinamica

--- src/app/pages/programa-interactivo/programa-interactivo.page.ts
+++ src/app/pages/programa-interactivo/programa-interactivo.page.ts
@@ -8,7 +8,8 @@ import { PickerOptions } from '@ionic/core';
 })
 export class ProgramaInteractivoPage implements OnInit {
   
-
+  fechaE = '';
+  contador = 0;
   fecha = new Date();
   dia = this.fecha.getDay();
   year = this.fecha.getFullYear();
@@ -18,7 +19,7 @@ export class ProgramaInteractivoPage implements OnInit {
   framework2 ='';
   framework3='';
   framework4='';
-  
+  frameworkEstado='';
   constructor( private pickerCtrl: PickerController) { }
 
   ngOnInit() {
@@ -38,11 +39,11 @@ export class ProgramaInteractivoPage implements OnInit {
         {
           name:'framework',
           options:[
-            {text:'TT Intermodal Querétaro',value:'A'},
-            {text:'TT Intermodal Guadalajara',value:'A'},
-            {text:'TT Intermodal Monterrey',value:'A'},
-            {text:'TT Intermodal Acolman',value:'A'},
-            {text:'TT Intermodal Tuxpan',value:'A'},
+            {text:'TT Intermodal Querétaro',value:'TT Intermodal Querétaro'},
+            {text:'TT Intermodal Guadalajara',value:'TT Intermodal Guadalajara'},
+            {text:'TT Intermodal Monterrey',value:'TT Intermodal Monterrey'},
+            {text:'TT Intermodal Acolman',value:'TT Intermodal Acolman'},
+            {text:'TT Intermodal Tuxpan',value:'TT Intermodal Tuxpan'},
           ]
         }
       ]
@@ -69,9 +70,9 @@ export class ProgramaInteractivoPage implements OnInit {
         {
           name:'framework2',
           options:[
-            {text:'Carro Tanque',value:'A'},
-            {text:'Buque Tanque',value:'A'},
-            {text:'Ducto',value:'A'},
+            {text:'Carro Tanque',value:'Carro Tanque'},
+            {text:'Buque Tanque',value:'Buque Tanque'},
+            {text:'Ducto',value:'Ducto'},
           ]
         }
       ]
@@ -82,7 +83,8 @@ export class ProgramaInteractivoPage implements OnInit {
       let col = await picker.getColumn('framework2');
       this.framework2=  col.options[ col.selectedIndex].text;
     });
-  }async picker3(){
+  }
+  async picker3(){
     let Opts: PickerOptions={
       buttons:[
         {
@@ -97,8 +99,8 @@ export class ProgramaInteractivoPage implements OnInit {
         {
           name:'framework3',
           options:[
-            {text:'Terminal',value:'A'},
-            {text:'Estacion de servicio',value:'A'},
+            {text:'Terminal',value:'Terminal'},
+            {text:'Estacion de servicio',value:'Estacion de servicio'},
           ]
         }
       ]
@@ -109,7 +111,9 @@ export class ProgramaInteractivoPage implements OnInit {
       let col = await picker.getColumn('framework3');
       this.framework3 =  col.options[ col.selectedIndex].text;
     });
-  }async picker4(){
+  }
+
+  async picker4(){
     let Opts: PickerOptions={
       buttons:[
         {
@@ -124,8 +128,8 @@ export class ProgramaInteractivoPage implements OnInit {
         {
           name:'framework4',
           options:[
-            {text:'Suministrador',value:'A'},
-            {text:'Cliente',value:'A'},
+            {text:'Suministrador',value:'Suministrador'},
+            {text:'Cliente',value:'ACliente'},
           ]
         }
       ]
@@ -135,8 +139,45 @@ export class ProgramaInteractivoPage implements OnInit {
     picker.onDidDismiss().then(async data=>{
       let col = await picker.getColumn('framework4');
       this.framework4 =  col.options[ col.selectedIndex].text;
+      console.log(this.framework4); 
     });
   }
 
-
+  async pickerEstado(){
+    let Opts: PickerOptions={
+      buttons:[
+        {
+          text:'Cancelar',
+          role:'cancel'
+        },
+        {
+          text:'Listo'
+        }
+      ],
+      columns:[
+        {
+          name:'frameworkEstado',
+          options:[
+            {text:'Cancelado',value:'Cancelado'},
+            {text:'Pendiente',value:'Pendiente'},
+            {text:'Activo',value:'Activo'},
+          ]
+        }
+      ]
+    };
+    let picker = await this.pickerCtrl.create(Opts);
+    picker.present();
+    picker.onDidDismiss().then(async data=>{
+      let col = await picker.getColumn('frameworkEstado');
+      this.frameworkEstado =  col.options[ col.selectedIndex].text;
+      console.log(this.frameworkEstado); 
+    });
+  }
+  
+  genera_tabla(){
+    this.contador ++;
+    console.log(this.contador);   
+    document.getElementById("wraped").insertRow(-1).innerHTML=`<td>${this.contador}</td><td>producto</td><td>M3.</td><td>${this.framework3}</td><td>${this.fechaE}</td><td>turno</td><td>PMX20000</td><td>1</td><td>20,000</td><td>${this.framework2}</td><td>${this.framework}</td><td>ESTIMADO</td><td>${this.frameworkEstado}</td><td>EL EMBARQUE SOBREPASA EL LIMITE DE LA TERMINAL</td>`
+    
+  }
 }
